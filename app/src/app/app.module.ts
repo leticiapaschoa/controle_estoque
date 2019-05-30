852e239f0e4e4fea4dc0038a72894068cbfe0c65
LETICIA PASCHOA DA SILVA <171150523@acad.dvuadmin.net>
2019-05-29 21:07:35 -0300

    Material Angular

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,15 +4,38 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import {FormsModule} from '@angular/forms';
+import {  
+  MatDialogModule,
+  MatInputModule,
+  MatIconModule,
+  MatCardModule,
+} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
   ],
-  imports: [
+  imports: [    
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatInputModule,
+    MatIconModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    CommonModule
+  ],
+  exports: [    
+    MatDialogModule,
+    MatInputModule,
+    MatIconModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
